33e35576273ed10c3ede725f8ddfa5864a93d129
Eganin <egorzaharin12@gmail.com>
2021-03-12 23:42:28 +0300

    add user repository

--- app/src/main/java/ru/academy/hackathon/data/repositories/UsersRepositoryImpl.kt
+++ app/src/main/java/ru/academy/hackathon/data/repositories/UsersRepositoryImpl.kt
@@ -1,34 +1,38 @@
 package ru.academy.hackathon.data.repositories
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import ru.academy.hackathon.data.models.User
+import ru.academy.hackathon.data.storage.UserDatabase
 
-class UsersRepositoryImpl : UserRepository {
+class UsersRepositoryImpl(applicationContext: Context) : UserRepository {
 
     private val dispatcher = Dispatchers.IO
 
-    override suspend fun getAllUsers(): LiveData<List<User>> {
-        TODO("Not yet implemented")
+    private val db = UserDatabase.getInstance(context = applicationContext)
+
+    private val dao = db.userDao
+
+    override suspend fun getAllUsers(): LiveData<List<User>> = dao.getAllUsers()
+
+    override suspend fun getUserById(id: Int): User = withContext(dispatcher) {
+        dao.getUserById(id = id)
     }
 
-    override suspend fun getUserById(id: Int): User {
-        TODO("Not yet implemented")
+    override suspend fun insertUser(user: User) = withContext(dispatcher) {
+        dao.insertUser(user = user)
     }
 
-    override suspend fun insertUser(user: User) {
-        TODO("Not yet implemented")
+    override suspend fun insertUsers(users: List<User>) = withContext(dispatcher) {
+        dao.insertUsers(users = users)
     }
 
-    override suspend fun insertUsers(users: List<User>) {
-        TODO("Not yet implemented")
+    override suspend fun deleteAllUsers() = withContext(dispatcher) {
+        dao.deleteAllUsers()
     }
 
-    override suspend fun deleteAllUsers() {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteUserById(id: Int) {
-        TODO("Not yet implemented")
+    override suspend fun deleteUserById(id: Int) = withContext(dispatcher) {
+        dao.deleteUserById(id = id)
     }
 }
--- app/src/main/java/ru/academy/hackathon/ui/addusers/AddUserFragment.kt
+++ app/src/main/java/ru/academy/hackathon/ui/addusers/AddUserFragment.kt
@@ -3,8 +3,10 @@ package ru.academy.hackathon.ui.addusers
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.academy.hackathon.R
 import ru.academy.hackathon.databinding.UsersFragmentBinding
+import ru.academy.hackathon.ui.adapters.AddUserAdapter
 
 class AddUserFragment : Fragment(R.layout.users_fragment) {
 
@@ -14,5 +16,17 @@ class AddUserFragment : Fragment(R.layout.users_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.addUser.setOnClickListener {
+
+        }
+
+    }
+
+    private  fun setupRecyclerView(){
+        binding.addUserRecyclerView.apply {
+            layoutManager=LinearLayoutManager(requireContext())
+            adapter = AddUserAdapter()
+        }
+        binding.addUserRecyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 }
